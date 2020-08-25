c7c3685ee2c48a988fe00a26056ed729da4f08be
Felipe Zêba <51774420+felipe-zg@users.noreply.github.com>
2020-08-25 20:26:06 -0300

    chore: added faker

--- src/data/useCases/authentication/remote-authentication.ts
+++ src/data/useCases/authentication/remote-authentication.ts
@@ -1,4 +1,4 @@
-import { HttpPostClient } from 'domain/data/protocols/http/http-post-client'
+import { HttpPostClient } from 'data/protocols/http/http-post-client'
 
 export class RemoteAuthentication {
   constructor(
