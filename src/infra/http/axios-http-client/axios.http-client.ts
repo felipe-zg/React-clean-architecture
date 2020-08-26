7513087774de88e9bebd7b1493b08e2d9af53aa7
felipe-zg <felipe_zeba@outlook.com>
2020-08-26 17:13:01 -0300

    feat: ensure AxiosHttpClient calls axios using the right method

--- src/infra/http/axios-http-client/axios.http-client.ts
+++ src/infra/http/axios-http-client/axios.http-client.ts
@@ -3,6 +3,6 @@ import axios from 'axios'
 
 export class AxiosHttpClient {
   async post (params: HttpPostClientParams<any>): Promise<void> {
-    await axios(params.url)
+    await axios.post(params.url)
   }
 }
