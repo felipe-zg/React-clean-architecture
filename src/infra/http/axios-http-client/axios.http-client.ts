2c00beb39f3da41d5e71a910af95cc7da751d750
felipe-zg <felipe_zeba@outlook.com>
2020-08-26 17:23:41 -0300

    feat: ensure AxiosHttpClient calls axios with correct bofy

--- src/infra/http/axios-http-client/axios.http-client.ts
+++ src/infra/http/axios-http-client/axios.http-client.ts
@@ -3,6 +3,6 @@ import axios from 'axios'
 
 export class AxiosHttpClient {
   async post (params: HttpPostClientParams<any>): Promise<void> {
-    await axios.post(params.url)
+    await axios.post(params.url, params.body)
   }
 }
