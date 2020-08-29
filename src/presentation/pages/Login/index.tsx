1729b86c4a3ae483a74941aebfcdc078614eb1fc
felipe-zg <felipe_zeba@outlook.com>
2020-08-28 21:31:37 -0300

    refactor: moved Input to a component

--- src/presentation/pages/Login/index.tsx
+++ src/presentation/pages/Login/index.tsx
@@ -3,6 +3,7 @@ import Spinner from '@/presentation/components/Spinner'
 import Styles from './styles.scss'
 import Header from '@/presentation/components/LoginHeader'
 import Footer from '@/presentation/components/Footer'
+import Input from '@/presentation/components/Input'
 
 const Login: React.FC = () => {
   return (
@@ -10,14 +11,8 @@ const Login: React.FC = () => {
       <Header/>
       <form className={Styles.form}>
         <h2>Login</h2>
-        <div className={Styles.inputWrap}>
-          <input type="email" name="email" placeholder="Digite seu e-mail"/>
-          <span className={Styles.status}>🔴</span>
-        </div>
-        <div className={Styles.inputWrap}>
-          <input type="password" name="password" placeholder="Digite sua senha"/>
-          <span className={Styles.status}>🔴</span>
-        </div>
+        <Input type="email" name="email" placeholder="Digite seu e-mail"/>
+        <Input type="password" name="password" placeholder="Digite sua senha"/>
         <button type="submit" className={Styles.submit}>Entrar</button>
         <span className={Styles.link}>Criar conta</span>
         <div className={Styles.errorWrap}>
