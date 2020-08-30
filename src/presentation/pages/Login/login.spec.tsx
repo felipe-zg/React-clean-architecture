431cfdca61c70871173e01ece7e15d4448e5fbf3
felipe-zg <felipe_zeba@outlook.com>
2020-08-30 12:11:22 -0300

    test: ensure Login will call validation with correct value on input change

--- src/presentation/pages/Login/login.spec.tsx
+++ src/presentation/pages/Login/login.spec.tsx
@@ -1,18 +1,35 @@
 import React from 'react'
-import { render, RenderResult } from '@testing-library/react'
+import { render, fireEvent, RenderResult, cleanup } from '@testing-library/react'
 import Login from '@/presentation/pages/Login'
+import { Validation } from '@/presentation/protocols/validation'
+import faker from 'faker'
 
 type SutTypes = {
   sut: RenderResult
+  validationSpy: ValidationSpy
+}
+
+class ValidationSpy implements Validation {
+  errorMessage: string
+  input: unknown
+
+  validate (input: unknown): string {
+    this.input = input
+    return this.errorMessage
+  }
 }
 const makeSut = (): SutTypes => {
-  const sut = render(<Login/>)
+  const validationSpy = new ValidationSpy()
+  const sut = render(<Login validation={validationSpy}/>)
   return {
-    sut
+    sut,
+    validationSpy
   }
 }
 
 describe('Login', () => {
+  beforeEach(cleanup)
+
   it('should not render spinner and error message on mount', () => {
     const { sut } = makeSut()
     const emailStatus = sut.getByTestId('email-status')
@@ -24,5 +41,13 @@ describe('Login', () => {
     expect(passwordStatus.title).toBe('Campo obrigatório')
     expect(passwordStatus.textContent).toBe('🔴')
     expect((sut.getByTestId('submit-button') as HTMLButtonElement).disabled).toBe(true)
+  })
+
+  it('', () => {
+    const { sut, validationSpy } = makeSut()
+    const email = faker.internet.email()
+    const emailInput = sut.getByTestId('email')
+    fireEvent.input(emailInput, { target: { value: email } })
+    expect(validationSpy.input).toEqual({ email })
   })
 })
