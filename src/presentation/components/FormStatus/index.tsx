d70927ba6a259c807af3377902ecd637ca755f0b
felipe-zg <felipe_zeba@outlook.com>
2020-08-30 05:44:17 -0300

    test: ensure Login page will not render spinner and error message on mount

--- src/presentation/components/FormStatus/index.tsx
+++ src/presentation/components/FormStatus/index.tsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import Context from '@/presentation/context/form/form-context'
 
 import Spinner from '@/presentation/components/Spinner'
-
 import Styles from './styles.scss'
 
 const FormStatus: React.FC = () => {
+  const { isLoading, errorMessage } = useContext(Context)
+
   return (
-    <div className={Styles.errorWrap}>
-      <Spinner className={Styles.spinner}/>
-      <span className={Styles.error}>Erro</span>
+    <div data-testid="error-wrap" className={Styles.errorWrap}>
+      {isLoading && <Spinner className={Styles.spinner}/>}
+      {errorMessage && <span className={Styles.error}>{errorMessage}</span>}
     </div>
   )
 }
--- src/presentation/pages/Login/login.spec.tsx
+++ src/presentation/pages/Login/login.spec.tsx
@@ -1,5 +1,10 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import Login from '@/presentation/pages/Login'
+
 describe('Login', () => {
   it('should not render spinner and error message on mount', () => {
-    expect(2).toBe(2)
+    const { getByTestId } = render(<Login/>)
+    expect(getByTestId('error-wrap').childElementCount).toBe(0)
   })
 })
