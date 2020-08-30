0abe11b93cabfd0ec10dbed845ea1a2619a4efbc
felipe-zg <felipe_zeba@outlook.com>
2020-08-30 08:37:32 -0300

    test: ensure Login starts with correct initial state values

--- src/presentation/pages/Login/login.spec.tsx
+++ src/presentation/pages/Login/login.spec.tsx
@@ -5,6 +5,14 @@ import Login from '@/presentation/pages/Login'
 describe('Login', () => {
   it('should not render spinner and error message on mount', () => {
     const { getByTestId } = render(<Login/>)
+    const emailStatus = getByTestId('email-status')
+    const passwordStatus = getByTestId('password-status')
+
     expect(getByTestId('error-wrap').childElementCount).toBe(0)
+    expect(emailStatus.title).toBe('Campo obrigatório')
+    expect(emailStatus.textContent).toBe('🔴')
+    expect(passwordStatus.title).toBe('Campo obrigatório')
+    expect(passwordStatus.textContent).toBe('🔴')
+    expect((getByTestId('submit-button') as HTMLButtonElement).disabled).toBe(true)
   })
 })
