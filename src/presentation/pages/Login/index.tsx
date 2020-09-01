c05afde68b3fca981a6d98c29965adeb97bab838
felipe-zg <felipe_zeba@outlook.com>
2020-09-01 03:06:21 -0300

    feat: ensure user will be redirected to the main page if authentication succeeds

--- src/presentation/pages/Login/index.tsx
+++ src/presentation/pages/Login/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 
 import Context from '@/presentation/context/form/form-context'
 
@@ -14,6 +14,7 @@ type Props = {
 }
 
 const Login: React.FC<Props> = ({ validation, authentication }: Props) => {
+  const history = useHistory()
   const [state, setState] = useState({
     isLoading: false,
     email: '',
@@ -41,6 +42,7 @@ const Login: React.FC<Props> = ({ validation, authentication }: Props) => {
       })
       const account = await authentication.auth({ email: state.email, password: state.password })
       localStorage.setItem('accessToken', account.accessToken)
+      history.replace('/')
     } catch (error) {
       setState({
         ...state,
