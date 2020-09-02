62946226490dbd46e1c99324daa4a2b8762d0e8e
felipe-zg <felipe_zeba@outlook.com>
2020-09-02 00:09:29 -0300

    test: ensure EmailValidation returns falsy if email is valid

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -1,10 +1,17 @@
+import faker from 'faker'
 import { InvalidFieldError } from '@/validation/erros/invalid-field-error'
 import { EmailValidation } from './email-validation'
 
 describe('Email validation', () => {
   it('Should return error if email is invalid', () => {
-    const sut = new EmailValidation('email')
-    const error = sut.validate('email')
+    const sut = new EmailValidation(faker.random.word())
+    const error = sut.validate(faker.random.word())
     expect(error).toEqual(new InvalidFieldError())
+  })
+
+  it('Should return falsy if email is valid', () => {
+    const sut = new EmailValidation(faker.random.word())
+    const error = sut.validate(faker.internet.email())
+    expect(error).toBeFalsy()
   })
 })
