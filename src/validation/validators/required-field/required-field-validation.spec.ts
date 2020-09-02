49d22be8e4a90acdc7e5b0fddebbe9a8255189ae
felipe-zg <felipe_zeba@outlook.com>
2020-09-01 23:46:22 -0300

    refactor: moved validators to a diferent folder

--- src/validation/validators/required-field/required-field-validation.spec.ts
+++ src/validation/validators/required-field/required-field-validation.spec.ts
@@ -1,16 +1,18 @@
 import faker from 'faker'
-import { RequiredFieldValidation } from '@/validation/required-field/required-field-validation'
+import { RequiredFieldValidation } from '@/validation/validators/required-field/required-field-validation'
 import { RequiredFieldError } from '@/validation/erros'
+
+const makeSut = (): RequiredFieldValidation => new RequiredFieldValidation(faker.database.column())
 
 describe('RequiredFieldValidation', () => {
   it('should return an error if field is empty', () => {
-    const sut = new RequiredFieldValidation('email')
+    const sut = makeSut()
     const error = sut.validate('')
     expect(error).toEqual(new RequiredFieldError())
   })
 
   it('should return falsy if field is not empty', () => {
-    const sut = new RequiredFieldValidation('email')
+    const sut = makeSut()
     const error = sut.validate(faker.random.word())
     expect(error).toBeFalsy()
   })
