28f9697f9441aac9a12652050f4f0bdb53243aa9
felipe-zg <felipe_zeba@outlook.com>
2020-09-01 23:58:32 -0300

    feat: ensure EmailValidation returns error if email is invalid

---
+++ src/validation/validators/email/email-validation.ts
@@ -0,0 +1,10 @@
+import { FieldValidation } from '@/validation/protocols/field-validation'
+import { InvalidFieldError } from '@/validation/erros/invalid-field-error'
+
+export class EmailValidation implements FieldValidation {
+  constructor(readonly field: string) {}
+
+  validate(value: string): Error {
+    return new InvalidFieldError()
+  }
+}
