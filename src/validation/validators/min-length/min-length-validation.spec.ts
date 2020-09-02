d2488f15dfd7e471643fd72e375ca33f05858e71
felipe-zg <felipe_zeba@outlook.com>
2020-09-02 00:45:09 -0300

    test: ensure MinLengthValidation returns falsy  if field length is valid

--- src/validation/validators/min-length/min-length-validation.spec.ts
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -3,9 +3,15 @@ import { MinLengthValidation } from './min-length-validation'
 import { InvalidFieldError } from '@/validation/erros/invalid-field-error'
 
 describe('MinLencthValidation', () => {
-  it('should return an error if field length is less than the required', () => {
+  it('should return an error if field length is invalid', () => {
     const sut = new MinLengthValidation(faker.random.word(), 5)
     const error = sut.validate(faker.random.alphaNumeric(4))
     expect(error).toEqual(new InvalidFieldError())
+  })
+
+  it('should return falsy if field length is valid', () => {
+    const sut = new MinLengthValidation(faker.random.word(), 5)
+    const error = sut.validate(faker.random.alphaNumeric(5))
+    expect(error).toBeFalsy()
   })
 })
