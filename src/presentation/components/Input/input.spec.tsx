06601da767aaa46116c636ed817837e76749d106
felipe-zg <felipe_zeba@outlook.com>
2020-09-03 01:44:58 -0300

    test: ensure Input makes readonly falsy on focus

--- src/presentation/components/Input/input.spec.tsx
+++ src/presentation/components/Input/input.spec.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import faker from 'faker'
-import { render, RenderResult } from '@testing-library/react'
+import { render, RenderResult, fireEvent } from '@testing-library/react'
 import { Input } from '@/presentation/components'
 import Context from '@/presentation/context/form/form-context'
 
@@ -17,5 +17,13 @@ describe('Input component', () => {
     const field = faker.database.column()
     const sut = makeSut(field)
     expect((sut.getByTestId(field) as HTMLInputElement).readOnly).toBe(true)
+  })
+
+  it('should make readonly falsy on input focus', () => {
+    const field = faker.database.column()
+    const sut = makeSut(field)
+    const input = sut.getByTestId(field) as HTMLInputElement
+    fireEvent.focus(input)
+    expect(input.readOnly).toBe(false)
   })
 })
