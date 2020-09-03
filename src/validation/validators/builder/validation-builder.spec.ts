61fd17fcd9418b65f0bf7b3679159ef1355cb021
felipe-zg <felipe_zeba@outlook.com>
2020-09-02 21:00:30 -0300

    test: ensure ValidationBuilder returns EmailValidation

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -1,11 +1,18 @@
 import faker from 'faker'
 import { ValidationBuilder as sut } from './validation-builder'
 import { RequiredFieldValidation } from '../required-field/required-field-validation'
+import { EmailValidation } from '../email/email-validation'
 
 describe('ValidationBuilder', () => {
   it('Should return RequiredFieldError', () => {
     const field = faker.database.column()
     const validators = sut.field(field).required().build()
     expect(validators).toEqual([new RequiredFieldValidation(field)])
+  })
+
+  it('Should return EmailValidation', () => {
+    const field = faker.database.column()
+    const validators = sut.field(field).email().build()
+    expect(validators).toEqual([new EmailValidation(field)])
   })
 })
