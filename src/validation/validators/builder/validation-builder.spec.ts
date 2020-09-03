664c5041ad338288ee1947713bfd4e1c696a0cc0
felipe-zg <felipe_zeba@outlook.com>
2020-09-02 21:03:23 -0300

    test: ensure ValidationBuilder returns MinLengthValidation

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -2,6 +2,7 @@ import faker from 'faker'
 import { ValidationBuilder as sut } from './validation-builder'
 import { RequiredFieldValidation } from '../required-field/required-field-validation'
 import { EmailValidation } from '../email/email-validation'
+import { MinLengthValidation } from '../min-length/min-length-validation'
 
 describe('ValidationBuilder', () => {
   it('Should return RequiredFieldError', () => {
@@ -14,5 +15,12 @@ describe('ValidationBuilder', () => {
     const field = faker.database.column()
     const validators = sut.field(field).email().build()
     expect(validators).toEqual([new EmailValidation(field)])
+  })
+
+  it('Should return MinLengthValidation', () => {
+    const field = faker.database.column()
+    const length = faker.random.number()
+    const validators = sut.field(field).min(length).build()
+    expect(validators).toEqual([new MinLengthValidation(field, length)])
   })
 })
