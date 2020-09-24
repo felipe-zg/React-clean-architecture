7cfe758a7317efe47982cf3f8aada03fe7d9618a
Felipe Zêba <51774420+felipe-zg@users.noreply.github.com>
2020-09-24 08:32:08 -0300

    test: ensure login shows valid state if form is valid

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -29,4 +29,17 @@ describe('Login', () => {
     cy.getByTestId('submit-button').should('have.attr', 'disabled')
     cy.getByTestId('error-wrap').should('not.have.descendants')
   })
+
+  it('should show valid stae if form is valid', () => {
+    cy.getByTestId('email').focus().type(faker.internet.email())
+    cy.getByTestId('email-status')
+      .should('have.attr', 'title', 'Tudo certo!')
+      .should('have.text', '🟢')
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(5))
+    cy.getByTestId('password-status')
+      .should('have.attr', 'title', 'Tudo certo!')
+      .should('have.text', '🟢')
+    cy.getByTestId('submit-button').should('not.have.attr', 'disabled')
+    cy.getByTestId('error-wrap').should('not.have.descendants')
+  })
 })
