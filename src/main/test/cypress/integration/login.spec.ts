ab5e60e4d72787110c177b9c8bebc6e1d2b72db8
Felipe Zêba <51774420+felipe-zg@users.noreply.github.com>
2020-09-24 07:58:59 -0300

    test: ensure login starts with correct state

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,5 +1,15 @@
 describe('Login', () => {
-  it('should start with correct state', () => {
+  beforeEach(() => {
     cy.visit('login')
+  })
+  it('should start with correct state', () => {
+    cy.getByTestId('email-status')
+      .should('have.attr', 'title', 'Campo obrigatório')
+      .should('have.text', '🔴')
+    cy.getByTestId('password-status')
+      .should('have.attr', 'title', 'Campo obrigatório')
+      .should('have.text', '🔴')
+    cy.getByTestId('submit-button').should('have.attr', 'disabled')
+    cy.getByTestId('error-wrap').should('not.have.descendants')
   })
 })
