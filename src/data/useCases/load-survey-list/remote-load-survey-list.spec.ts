0ab19fb90342479947bf86a1648bea12393e6be4
Felipe Zêba <51774420+felipe-zg@users.noreply.github.com>
2020-09-26 08:15:56 -0300

    test: ensure RemoteLoadSurveyList throws Unexpected error if HttpGetClients returns 500

--- src/data/useCases/load-survey-list/remote-load-survey-list.spec.ts
+++ src/data/useCases/load-survey-list/remote-load-survey-list.spec.ts
@@ -35,4 +35,13 @@ describe('RemoteLoadSurveyList', () => {
     const promise = sut.loadAll()
     await expect(promise).rejects.toThrow(new UnexpectedError())
   })
+
+  it('should throw UnexpectedError if HttpGetClient returns 500', async () => {
+    const { sut, httpGetClientSpy } = makeSut()
+    httpGetClientSpy.response = {
+      statusCode: HttpStatusCode.serverError
+    }
+    const promise = sut.loadAll()
+    await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
 })
