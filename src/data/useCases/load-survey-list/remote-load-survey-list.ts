72aeffb616a9a28dc437250e99afc9efd08a117d
Felipe Zêba <51774420+felipe-zg@users.noreply.github.com>
2020-09-26 08:41:35 -0300

    feat: ensure RemoteLoadSurveyList returns a list of Surveys if HttpGetClient returns 200

--- src/data/useCases/load-survey-list/remote-load-survey-list.ts
+++ src/data/useCases/load-survey-list/remote-load-survey-list.ts
@@ -1,18 +1,20 @@
 import { HttpStatusCode } from '@/data/protocols/http'
 import { HttpGetClient } from '@/data/protocols/http/http-get-client'
 import { UnexpectedError } from '@/domain/errors'
+import { SurveyModel } from '@/domain/models'
+import { LoadSurveyList } from '@/domain/use-cases'
 
-export class RemoteLoadSurveyList {
+export class RemoteLoadSurveyList implements LoadSurveyList {
   constructor(
     private readonly url: string,
-    private readonly httpGetClient: HttpGetClient
+    private readonly httpGetClient: HttpGetClient<SurveyModel[]>
   ) {}
 
-  async loadAll(): Promise<void> {
+  async loadAll(): Promise<SurveyModel[]> {
     const httpResponse = await this.httpGetClient.get({ url: this.url })
     switch (httpResponse.statusCode) {
       case HttpStatusCode.ok:
-        break
+        return httpResponse.body
       default:
         throw new UnexpectedError()
     }
