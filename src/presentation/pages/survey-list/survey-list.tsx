9a5324a5f7f6709d264d7328797efc4cb67bd12c
Felipe Zêba <51774420+felipe-zg@users.noreply.github.com>
2020-09-26 14:53:21 -0300

    refactor: adapted layout to be responsive

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -26,6 +26,30 @@ const SurveyList: React.FC = () => {
             </div>
             <footer>Ver resultado</footer>
           </li>
+          <li>
+            <div className={Styles.surveyContent}>
+              <Icon iconName={IconName.thumbsUp} className={Styles.iconWrap} />
+              <time>
+                <span className={Styles.day}>22</span>
+                <span className={Styles.month}>03</span>
+                <span className={Styles.year}>2020</span>
+              </time>
+              <p>Qual é o seu framework web favorito?</p>
+            </div>
+            <footer>Ver resultado</footer>
+          </li>
+          <li>
+            <div className={Styles.surveyContent}>
+              <Icon iconName={IconName.thumbsUp} className={Styles.iconWrap} />
+              <time>
+                <span className={Styles.day}>22</span>
+                <span className={Styles.month}>03</span>
+                <span className={Styles.year}>2020</span>
+              </time>
+              <p>Qual é o seu framework web favorito?</p>
+            </div>
+            <footer>Ver resultado</footer>
+          </li>
         </ul>
       </div>
       <Footer />
