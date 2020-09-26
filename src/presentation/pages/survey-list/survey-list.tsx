b67442eac77fd70797ad806c973a249f7408dffb
Felipe Zêba <51774420+felipe-zg@users.noreply.github.com>
2020-09-26 14:27:42 -0300

    refactor: moved Survey status icon to a separate component

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
-import { Footer, LoggedInHeader } from '@/presentation/components'
-import { ThumbsDown } from '@/presentation/assets/icons/base-64/thumbs-down'
+import {
+  Footer,
+  Icon,
+  IconName,
+  LoggedInHeader
+} from '@/presentation/components'
 import Styles from './styles.scss'
 
 const SurveyList: React.FC = () => {
@@ -12,9 +16,7 @@ const SurveyList: React.FC = () => {
         <ul>
           <li>
             <div className={Styles.surveyContent}>
-              <div className={[Styles.iconWrap, Styles.red].join(' ')}>
-                <img src={ThumbsDown} />
-              </div>
+              <Icon iconName={IconName.thumbsUp} className={Styles.iconWrap} />
               <time>
                 <span className={Styles.day}>22</span>
                 <span className={Styles.month}>03</span>
