7fa34c837abc0520b527451b54138735818f3830
Felipe Zêba <51774420+felipe-zg@users.noreply.github.com>
2020-09-28 13:59:52 -0300

    test: ensure LocalStorageAdapter calls get with correct value

--- src/main/adapters/currenct-account-adapter.spec.ts
+++ src/main/adapters/currenct-account-adapter.spec.ts
@@ -1,10 +1,13 @@
 import { UnexpectedError } from '@/domain/errors'
 import { mockAccountModel } from '@/domain/test'
 import { LocalStorageAdapter } from '@/infra/cache/local-storage-adapter'
-import { setCurrentAccountAdapter } from './current-account-adapter'
+import {
+  setCurrentAccountAdapter,
+  getCurrentAccountAdapter
+} from './current-account-adapter'
 
 describe('CurrentAccountAdapter', () => {
-  it('should call LocalStorageAdapter with correct values', () => {
+  it('should call LocalStorageAdapter.set with correct values', () => {
     const account = mockAccountModel()
     const setSpy = jest.spyOn(LocalStorageAdapter.prototype, 'set')
     setCurrentAccountAdapter(account)
@@ -15,5 +18,15 @@ describe('CurrentAccountAdapter', () => {
     expect(() => {
       setCurrentAccountAdapter(undefined)
     }).toThrow(new UnexpectedError())
+  })
+
+  it('should call LocalStorageAdapter.get with correct value', () => {
+    const account = mockAccountModel()
+    const getSpy = jest
+      .spyOn(LocalStorageAdapter.prototype, 'get')
+      .mockReturnValueOnce(account)
+    const result = getCurrentAccountAdapter()
+    expect(getSpy).toHaveBeenCalledWith('account')
+    expect(result).toEqual(account)
   })
 })
